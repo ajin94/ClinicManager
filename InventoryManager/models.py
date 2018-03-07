b41d873a9a173765dbeca1c5cbbd3380027d2335
ajin94 <ajinsanut@gmail.com>
2018-03-07 15:10:39 +0530

    updates

--- InventoryManager/models.py
+++ InventoryManager/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Inventory(models.Model): pass
+
+
+class Purchase(models.Model): pass
+
+
+class Sales(models.Model): pass
--- SkinClinic/settings.py
+++ SkinClinic/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'InventoryManager.apps.InventorymanagerConfig',
     'PatientLedger.apps.PatientledgerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
