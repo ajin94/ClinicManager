333a6b8fcf16fac32f1c50b776ad336fe969da38
ajin94 <ajinsanut@gmail.com>
2018-03-08 15:57:15 +0530

    updated

--- InventoryManager/models.py
+++ InventoryManager/models.py
@@ -4,21 +4,21 @@ from django.db import models
 
 
 class MedicalCompany(models.Model):
-    name = models.CharField(unique=True, null=False, blank=False)
+    name = models.CharField(max_length=200, unique=True, null=False, blank=False)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
 
 class Branding(models.Model):
-    name = models.CharField(unique=True, null=False, blank=False)
+    name = models.CharField(max_length=200, unique=True, null=False, blank=False)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
 
 class Purchase(models.Model):
     medicine_name = models.CharField(max_length=250, null=False, blank=False)
-    company_name = models.ForeignKey(MedicalCompany, null=True, blank=True)
-    brand_name = models.ForeignKey(Branding, null=True, blank=True)
+    company_name = models.ForeignKey(MedicalCompany, null=True, blank=True, on_delete=models.DO_NOTHING)
+    brand_name = models.ForeignKey(Branding, null=True, blank=True, on_delete=models.DO_NOTHING)
     quantity = models.IntegerField(null=False, blank=False, default=0)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
