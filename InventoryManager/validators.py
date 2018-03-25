e9191c24dd1ff03fd247c855d450695f612b61c8
ajin94 <ajinsanut@gmail.com>
2018-03-25 22:42:45 +0530

    updates with foundation ui

--- InventoryManager/validators.py
+++
@@ -1,9 +0,0 @@
-from django.core.exceptions import ValidationError
-from django.utils.translation import ugettext_lazy as _
-
-
-def validate_sales_quantity(quantity):
-    if quantity > 10:
-        raise ValidationError(_("Quantity exceeds stock"))
-    else:
-        pass
